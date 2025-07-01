8e66aeb8044e92c48b1e2d8ab2cdd87a27b44124
Tomik <maxiwrayt@gmail.com>
2025-07-01 11:03:28 +0200

    feat(user): add User and Profile models

--- users/models.py
+++ users/models.py
@@ -1,3 +1,35 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class User(AbstractUser):
+    first_name = None
+    last_name = None
+    is_staff = None
+
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=30, unique=True)
+
+    def __str__(self):
+        return f"User: {self.username}"
+
+    class Meta:
+        db_table = "user"
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="profile",
+        primary_key=True,
+    )
+    bio = models.TextField("biography", blank=True, default="")
+    avatar = models.ImageField(upload_to="avatars/", blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s profile"
+
+    class Meta:
+        db_table = "user_profile"
