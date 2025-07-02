b5d1ddb1fae34c1fa7586634834e476f62d299e3
Tomik <maxiwrayt@gmail.com>
2025-07-02 12:06:55 +0200

    feat(communities): add Community and Subscription models

--- communities/models.py
+++ communities/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
+class Community(models.Model):
+    name = models.CharField(max_length=30, blank=False, null=False, unique=True)
+    description = models.TextField(blank=True, null=False, default="")
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(to=User, on_delete=models.SET_NULL, blank=True, null=True, db_index=True)
+
+    def __str__(self):
+        return f"Community: {self.name}"
+
+    class Meta:
+        db_table = "community"
+        ordering = ["name"]
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    community = models.ForeignKey(to=Community, on_delete=models.CASCADE)
+    subscribed_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} subscribed to {self.community.name}"
+
+    class Meta:
+        db_table = "subscription"
+        constraints = [
+            models.UniqueConstraint(fields=["user", "community"], name="unique_subscription")
+        ]
+        
+        
