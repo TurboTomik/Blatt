1ae481da25f0878af1b9e34ad3f2af18f9e8fcfe
Tomik <maxiwrayt@gmail.com>
2025-09-29 21:40:28 +0200

    fix: update LOGIN_URL to use a correct path

--- core/settings.py
+++ core/settings.py
@@ -146,7 +146,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "users.User"
 
 LOGIN_REDIRECT_URL = "/"
-LOGIN_URL = "users:sign-in"
+LOGIN_URL = "sign-in"
 LOGOUT_REDIRECT_URL = "/"
 
 REGISTER_REDIRECT_URL = "/"
