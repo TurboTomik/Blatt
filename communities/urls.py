ad9ddf4945317e67636df90b8d61135428549f64
Tomik <maxiwrayt@gmail.com>
2025-10-01 12:12:52 +0200

    feat: add post detail view and URL routing

--- communities/urls.py
+++ communities/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import include, path
 
 from . import views
 
 urlpatterns = [
     path("create/", views.CreateCommunityView.as_view(), name="community-create"),
     path("<str:name>/", views.CommunityDetailView.as_view(), name="community-detail"),
+    path("<str:name>/post/", include("posts.urls")),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import CreateView, DetailView
 
-# Create your views here.
+from .models import Post
+
+
+class PostDetailView(DetailView):
+    """Display the details of a single Post instance."""
+
+    model = Post
+    context_object_name = "post"
