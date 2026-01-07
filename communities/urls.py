e5a786cbc2611b72957aaa93d758921a5fff3387
Tomik <maxiwrayt@gmail.com>
2026-01-07 20:37:03 +0100

    feat(communities): add ability to join or leave communityj
    Add JoinView and LeaveView, as well as their respective URLs.

--- communities/urls.py
+++ communities/urls.py
@@ -5,5 +5,9 @@ from . import views
 urlpatterns = [
     path("create/", views.CreateCommunityView.as_view(), name="community-create"),
     path("<str:name>/", views.CommunityDetailView.as_view(), name="community-detail"),
+    path("<str:name>/join/", views.CommunityJoinView.as_view(), name="community-join"),
+    path(
+        "<str:name>/leave/", views.CommunityLeaveView.as_view(), name="community-leave"
+    ),
     path("<str:name>/post/", include("posts.urls")),
 ]
--- communities/views.py
+++ communities/views.py
@@ -1,9 +1,15 @@
+from typing import Any
+
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.forms import ModelForm
-from django.http import HttpResponseRedirect
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, redirect
 from django.views.generic import CreateView, DetailView
+from django.views.generic.base import View
 
-from .models import Community
+from posts.models import Post
+
+from .models import Community, Subscription
 
 
 class CreateCommunityView(LoginRequiredMixin, CreateView):
@@ -24,3 +30,64 @@ class CommunityDetailView(DetailView):
     model = Community
     slug_field = "name"
     slug_url_kwarg = "name"
+class CommunityJoinView(LoginRequiredMixin, View):
+    """Handle a POST request that allows an authenticated user to join a community."""
+
+    def post(self, request: HttpRequest, **kwargs: Any) -> HttpResponse:
+        """
+        Add the current authenticated user to the specified community.
+
+        URL kwargs:
+            name (str): The unique name of the community to join.
+
+        Args:
+            request (HttpRequest): The incoming HTTP request.
+            *args: Positional arguments passed to the view.
+            **kwargs: Keyword arguments containing URL parameters.
+
+        Returns:
+            HttpResponse: A redirect response to the community detail page.
+        """
+        community_name = kwargs["name"]
+
+        community = get_object_or_404(
+            Community,
+            name=community_name,
+        )
+
+        Subscription.objects.subscribe_user(
+            user=request.user,
+            community=community,
+        )
+
+        return redirect("community-detail", name=community_name)
+
+
+class CommunityLeaveView(LoginRequiredMixin, View):
+    """Handle a POST request that allows an authenticated user to leave a community."""
+
+    def post(self, request: HttpRequest, **kwargs: Any) -> HttpResponse:
+        """
+        Delete the current authenticated user to the specified community.
+
+        URL kwargs:
+            name (str): The unique name of the community to leave.
+
+        Args:
+            request (HttpRequest): The incoming HTTP request.
+            *args: Positional arguments passed to the view.
+            **kwargs: Keyword arguments containing URL parameters.
+
+        Returns:
+            HttpResponse: A redirect response to the community detail page.
+        """
+        community_name = kwargs["name"]
+
+        community = get_object_or_404(Community, name=community_name)
+
+        Subscription.objects.unsubscribe_user(
+            user=request.user,
+            community=community,
+        )
+
+        return redirect("community-detail", name=community_name)
